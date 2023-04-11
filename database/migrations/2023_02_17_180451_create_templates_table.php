b4723ff8df74649a42e40764fa8f8d3060bb2e53
Dr.Pvssy <medchihab651@gmail.com>
2023-04-11 05:25:28 +0200

    migration

---
+++ database/migrations/2023_02_17_180451_create_templates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->id();
+            $table->string('cover');
+            $table->text('template_code');
+            $table->string('is_free');
+            $table->integer('downloads')->default(0);
+            $table->foreignId('created_by')->constrained('users', 'id')->cascadeOnUpdate();
+            $table->foreignId('category_id')->constrained()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('templates');
+    }
+};
